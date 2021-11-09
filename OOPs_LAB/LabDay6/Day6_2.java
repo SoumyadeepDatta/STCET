8488f898bdd76b0efd123b0e1c1a552792ca03b7
Soumyadeep Datta <85609404+SoumyadeepDatta@users.noreply.github.com>
2021-11-09 22:21:11 +0530

    Create Day6_2.java

---
+++ OOPs_LAB/LabDay6/Day6_2.java
@@ -0,0 +1,11 @@
+
+public class Day6_2 {
+	static {
+		System.out.println("Static block is going to be called even before the main() method");
+	};
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is inside main() method");
+	}
+
+}
