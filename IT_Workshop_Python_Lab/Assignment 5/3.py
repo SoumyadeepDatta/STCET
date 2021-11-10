ee6ded4389cd1a826524172afa924be96b3a6399
Soumyadeep Datta <85609404+SoumyadeepDatta@users.noreply.github.com>
2021-11-10 20:42:18 +0530

    Create 3.py

---
+++ IT_Workshop_Python_Lab/Assignment 5/3.py
@@ -0,0 +1,16 @@
+for num in range(0, 1000 + 1):
+
+
+    order = len(str(num))
+
+
+    sum = 0
+
+    temp = num
+    while temp > 0:
+        digit = temp % 10
+        sum += digit ** order
+        temp //= 10
+
+    if num == sum:
+        print(num)
