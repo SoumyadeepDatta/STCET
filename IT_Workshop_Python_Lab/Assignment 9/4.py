7c16c3a142a1ffbd293530ef6201361054207480
Soumyadeep Datta <85609404+SoumyadeepDatta@users.noreply.github.com>
2021-11-10 22:35:23 +0530

    Create 4.py

---
+++ IT_Workshop_Python_Lab/Assignment 9/4.py
@@ -0,0 +1,19 @@
+students=("Arun","Rajesh","Harish","Akansha","Lakshmi","Varsha")
+
+
+
+print(students[1:4]) #[star index:end index+1]
+
+print(students[:]) #print all
+
+print(students[3:]) #print from after index 3
+
+print(students[:5]) #print upto index 5
+
+print(students[1:5:2]) #print upto index 4 skipping 1 step
+
+print(students[1:]) #print from after index 1
+
+print(students[:-1])  #print upto index 5
+
+print(students[6:2:-2]) #print from index 6 upto index 3 skipping 1 step
