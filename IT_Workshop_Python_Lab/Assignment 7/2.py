7a508aec5dc3dc0efe1db576da882d7de0736832
Soumyadeep Datta <85609404+SoumyadeepDatta@users.noreply.github.com>
2021-11-10 21:07:03 +0530

    Create 2.py

---
+++ IT_Workshop_Python_Lab/Assignment 7/2.py
@@ -0,0 +1,8 @@
+ages=[18,19,20,20,12,14,18,21,15,18,19]
+print("Freq of 12 ",ages.count(12))
+print("Freq of 14 ",ages.count(14))
+print("Freq of 15 ",ages.count(15))
+print("Freq of 18 ",ages.count(18))
+print("Freq of 19 ",ages.count(19))
+print("Freq of 20 ",ages.count(20))
+print("Freq of 21 ",ages.count(21))
