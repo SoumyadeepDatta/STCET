9e38c259a35f97d763b7311990f29a0444389e36
Soumyadeep Datta <85609404+SoumyadeepDatta@users.noreply.github.com>
2021-11-10 16:50:36 +0530

    Create 6.py

---
+++ IT_Workshop_Python_Lab/Assignment 1/6.py
@@ -0,0 +1,12 @@
+print("Complex no. arithmetic\n\n")
+
+
+print("Addition  : ",(5+3j)+(5-7j))
+print("Subtraction  : ",(9+3j)-(2-7j))
+print("Multiplication  : ",(11+3j)*(8-7j))
+print("Division  : ",(6+3j)/(4-7j))
+
+z= complex(23,-12)
+print(z)
+print(z.real)
+print(z.imag)
