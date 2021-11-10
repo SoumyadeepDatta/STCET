251428dde8ed630ebf34bdeb93338e974d7cab0f
Soumyadeep Datta <85609404+SoumyadeepDatta@users.noreply.github.com>
2021-11-10 17:11:20 +0530

    Create 3.py

---
+++ IT_Workshop_Python_Lab/Assignment 4/3.py
@@ -0,0 +1,11 @@
+x=input("Enter a number: ")
+print(x*3)
+
+
+'''
+By default python takes inputs as a string.
+So it takes 25 as string and when multiplied by
+3 it simply prints 3 times the string. i.e
+it becomes '252525'
+Similar as 'a'*3 will print 'aaa'
+'''
