e5cf22d8d85ff0723dd218e8d8551d4d89fc0f9a
Soumyadeep Datta <85609404+SoumyadeepDatta@users.noreply.github.com>
2021-11-10 17:12:13 +0530

    Update 3.py

--- IT_Workshop_Python_Lab/Assignment 4/3.py
+++ IT_Workshop_Python_Lab/Assignment 4/3.py
@@ -3,9 +3,5 @@ print(x*3)
 
 
 '''
-By default python takes inputs as a string.
-So it takes 25 as string and when multiplied by
-3 it simply prints 3 times the string. i.e
-it becomes '252525'
-Similar as 'a'*3 will print 'aaa'
+By default python takes inputs as a string. So it takes 25 as string and when multiplied by 3 it simply prints 3 times the string. i.e it becomes '252525' Similar as 'a'*3 will print 'aaa'
 '''
