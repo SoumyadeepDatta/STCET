5baf9954d8ff5da64e363d2615df2eeaa3a4b322
Soumyadeep Datta <85609404+SoumyadeepDatta@users.noreply.github.com>
2021-11-11 12:58:02 +0530

    Create 5.py

---
+++ IT_Workshop_Python_Lab/Assignment 11/5.py
@@ -0,0 +1,11 @@
+def box(length=1,width=1,ht=1):
+    return length*width*ht
+
+
+print("Default value is" ,box())
+
+print(box(2))    #1 arg
+
+print(box(2,50))    #2 args
+
+print(box(2,50,10))     #3 args
