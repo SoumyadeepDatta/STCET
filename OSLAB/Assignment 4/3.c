5ec8c3b6275f542c038ac83cb1be42ef23e6b6fb
Soumyadeep Datta <85609404+SoumyadeepDatta@users.noreply.github.com>
2021-12-09 20:46:43 +0530

    Update 3.c

--- OSLAB/Assignment 4/3.c
+++ OSLAB/Assignment 4/3.c
@@ -1,22 +1,11 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<unistd.h>
-#include<sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-int main(void)
+int main()
 {
-    pid_t pid;
-    for(int i=0;i<8;i++) // loop will run 8 times to create 8 child processes
-    {
-        pid = fork();
-        if(pid == 0)
-        {
-            printf("PARENT --PID: %d CHILD --PID: %d\n",getppid(),getpid());
-            exit(0);
-        }
-    }
-    for(int i=0;i<8;i++) // loop will run 8 times to terminate the child processes
-    {
-    	wait(NULL);
-    } 
+    fork();
+    fork();
+    fork();
+    printf("Hello World\n"); // Binary Tree structure
 }
