52c7f2614905234d4fd7f547374b2bc6a910c09e
Soumyadeep Datta <85609404+SoumyadeepDatta@users.noreply.github.com>
2022-01-06 17:35:05 +0530

    Create selection_sort.c

---
+++ DAA_Lab/Divide and conquer method/selection_sort.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+void selection(int a[],int n)
+{
+	int t,min,k,i,pos,flag;
+	for(k=0;k<n;k++)
+	{
+		min=a[k];
+		flag=0;
+		for(i=k+1;i<n;i++)
+		{
+			if(a[i]<min)
+			{
+				min=a[i];
+				pos=i;
+				flag=1;
+			}			
+		}
+		if(flag==1)
+		{
+			t=a[pos];
+			a[pos]=a[k];
+			a[k]=t;
+		}
+	}
+}
+int main()
+{
+	int i,n;
+    printf("Enter range:");
+    scanf("%d",&n);
+    printf("Enter elements:");
+    int arr[n];
+    for(i=0;i<n;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	selection(arr,n);
+    printf("\n");
+	for(i=0;i<n;i++)
+	{
+		printf("%d\t",arr[i]);
+	}
+	return 0;
+}
