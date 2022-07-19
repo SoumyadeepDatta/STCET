415f5b097709c17a5d17326c3fb3581029613791
Soumyadeep Datta <85609404+SoumyadeepDatta@users.noreply.github.com>
2022-07-19 12:45:41 -0700

    Update hamiltonian.c

--- DAA_Lab/backtracking/hamiltonian.c
+++ DAA_Lab/backtracking/hamiltonian.c
@@ -3,19 +3,20 @@
 int mat[M][M];
 int x[M];
 int path=1;
-void nextValue(int k,int no)
+void nextValue(int k,int n)
 {
-    int j;
+
     while(1)
     {
-        x[k]=(x[k]+1)%(no+1);
+        x[k]=(x[k]+1)%(n+1);
         if(x[k]==0)
         {
             return;
         }
         if(mat[x[k-1]][x[k]] != 0)
         {
-            for(j=1;j<=k-1;j++)
+            int j;
+            for(j=0;j<=k-1;j++)
             {
                 if(x[j]==x[k])
                 {
@@ -24,38 +25,39 @@ void nextValue(int k,int no)
             }
                 if(j==k)
                 {
-                    if((k<no) || ((k==no) && (mat[x[no]][x[1]] != 0)))
+                    if((k<n) || ((k==n-1) && (mat[x[k]][x[0]] != 0)))
                     {
                         return;
                     }
                 }
-            
+
         }
     }
 }
 
-void hamiltonian(int k, int no)
+void hamiltonian(int k, int n)
 {
-    int t;
+    int path=0;
     while(1)
     {
-        nextValue(k,no);
+        nextValue(k,n);
         if(x[k]==0)
         {
             return;
         }
-        if(k==no)
+        if(k==n-1)
         {
-            printf("\n\nPath %d\n\n",path);
-            for(t=1;t<=no;t++)
+            //printf("\n\nPath %d\n\n",path);
+            for(int t=0;t<n;t++)
             {
-                printf("V%d\t",x[t]);
+                printf("V%d\t",x[t]+1);
             }
+            printf("V%d\t\n",x[0]+1);
             path++;
         }
         else
         {
-            hamiltonian(k+1,no);
+            hamiltonian(k+1,n);
         }
     }
 }
@@ -63,14 +65,19 @@ void hamiltonian(int k, int no)
 int main()
 {
     int n,i,j,c,h;
-    
-    printf("\nEnter no. of vertices :");
+
+    printf("\nEnter n. of vertices :");
     scanf("%d",&n);
+    int x[n];
+for(int i=0;i<n;i++)
+{
+x[i]=0;
+}
     printf("\nEnter 1(if edge exists) else 0:");
     // int mat[n+1][n+1];
-    for(i=1;i<=n;i++)
+    for(i=0;i<n;i++)
     {
-        for(j=1;j<=n;j++)
+        for(j=0;j<n;j++)
         {
             if(i==j)
             {
@@ -84,12 +91,13 @@ int main()
                 }
                 else
                 {
-                    printf("\n%d to %d :",i,j);
+                    printf("\n%d to %d :",i+1,j+1);
                     scanf("%d",&mat[i][j]);
                 }
             }
         }
     }
+    x[0]=0;
     hamiltonian(1,n);
     return 0;
 }
